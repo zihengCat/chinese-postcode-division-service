ca46e412c314c87d7a01e3888f8bb70c0c823dcf
ziheng <zihenglv@gmail.com>
2018-08-01 21:36:14 +0800

    [modify]: main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import chinese_postcode_division_service as cpds
 if __name__ == '__main__':
     c = cpds.PostCodeDivisionService()
-    print(c.parseCode('310053'))
-    print(c.parseCode('317100'))
-
+    # 正确用例 =>
+    print(len(c.parseCode('310053')))
+    print(c.parseCode('317100')[0])
+    # 错误用例 =>
+    print(c.parseCode('000000'))
