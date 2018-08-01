c92194b150fe97eed4c58764fa52e1c91fb7d3ce
ziheng <zihenglv@gmail.com>
2018-08-01 20:57:14 +0800

    [add]: .travis.py

--- main.py
+++ main.py
@@ -2,5 +2,6 @@
 import chinese_postcode_division_service as cpds
 if __name__ == '__main__':
     c = cpds.PostCodeDivisionService()
+    print(c.parseCode('310053'))
     print(c.parseCode('317100'))
 
